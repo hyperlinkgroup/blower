a3e83b68ea9e6f344eb7798a1f0b036268f277e2
Felix Scholze <felix.scholze@hyperlink.eu>
2024-06-24 14:27:00 +0200

    test(BLW-33): force rerun on scss file changes

--- vitest.config.js
+++ vitest.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, configDefaults } from 'vitest/config'
 
 export default defineConfig ({
 	test: {
 		watch: false,
 		globals: true,
-	},
+		forceRerunTriggers: [...configDefaults.forceRerunTriggers ,'**/*.scss/**']
+	}
 })
